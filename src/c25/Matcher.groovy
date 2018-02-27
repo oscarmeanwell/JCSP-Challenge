2813c553cf377b3c29b8a70293ea1d98e630389e
Jostein Dyrseth <josdyr@gmail.com>
2018-02-27 19:00:40 +0000

    Add files via upload

--- src/c25/Matcher.groovy
+++ src/c25/Matcher.groovy
@@ -1,6 +1,6 @@
-package c25
+package c25_local
 
-import jcsp.groovy.*
+import groovyJCSP.*
 import jcsp.lang.*
 
 class Matcher implements CSProcess {
@@ -9,31 +9,36 @@ class Matcher implements CSProcess {
 	ChannelInput receivePoint
 	ChannelOutput getPoint
 	
-	def getXY = { point, side, gap ->
+	def getXY = { point, side, gap -> // returns [x, y]
 		def int x = (point[0] - gap) / (side + gap)
 		def int y = (point[1] - gap) / (side + gap)
 		return [x, y]
-	} //returns [x, y] coordinates
+	}
 	
 	void run (){
 		while (true){
-			def getData = (GetValidPoint)getValidPoint.read() //read from getValidPoint
-			def pairsMap = getData.pairsMap
+			def getData = (GetValidPoint)getValidPoint.read() //1st: get valid point from PM
+			def pairsMap = getData.pairsMap //PairsMap from PM
 			def side = getData.side
 			def gap = getData.gap
-			//println  "Matcher - $side, $gap"
-			//pairsMap.each {println"${it}"}
+			
+			//some print magic
+			println  "Matcher - $side, $gap"
+			pairsMap.each {println"${it}"}
+			
 			def gotValidPoint = false
 			def pointXY 
-			while (!gotValidPoint){
-				getPoint.write(0) //signals that it has not got a point?
-				def point = ((MousePoint)receivePoint.read()).point
-				pointXY = getXY(point, side, gap)
-				//println  "point = $point; pointXY = $pointXY"
-				if ( pairsMap.containsKey(pointXY)) gotValidPoint = true
+			
+			while (!gotValidPoint){ //getValidPoint
+				getPoint.write(0) //2nd: signals a request to MouseBuffer
+				def point = ((MousePoint)receivePoint.read()).point // 3rd: read from MouseBuffer and store point as MousePoint
+				pointXY = getXY(point, side, gap) //store pointXY as [x, y]
+				println  "point = $point; pointXY = $pointXY"
+				if ( pairsMap.containsKey(pointXY)) gotValidPoint = true //if pairsMap contains the point, set gotValidPoint to true
 			}
-			//println  "Matcher: pointXY = $pointXY"
-			validPoint.write(new SquareCoords(location: pointXY))
+			
+			println  "Matcher: pointXY = $pointXY"
+			validPoint.write(new SquareCoords(location: pointXY)) //write to the PM a new SquareCoords with it's location to the [x, y] data.
 		} // end while
 		
 	} // end run
--- src/c25/MouseBuffer.groovy
+++ src/c25/MouseBuffer.groovy
@@ -1,6 +1,6 @@
-package c25
+package c25_local
 
-import jcsp.groovy.*
+import groovyJCSP.*
 import jcsp.lang.*
 import java.awt.event.*
 import java.awt.event.MouseEvent
@@ -11,26 +11,26 @@ class MouseBuffer implements CSProcess {
 	ChannelOutput sendPoint
 	
 	void run(){
-		def alt = new ALT([getPoint, mouseEvent])
+		def alt = new ALT([getPoint, mouseEvent]) // TODO: we probably need to change the priority here??? first read from PI, then send to Matcher. @josdyr
 		def preCon = new boolean[2]
 		def GET = 0
-		preCon[GET] = false
-		preCon[1] = true
+		preCon[GET] = false // getPoint
+		preCon[1] = true // mouse event
 		def point
 		
 		while (true){
 			switch ( alt.select(preCon)) {
 				case GET :
-					getPoint.read()
-					sendPoint.write(new MousePoint (point: point))
-					preCon[GET] = false
+					getPoint.read() //1st: read from getPoint
+					sendPoint.write(new MousePoint (point: point)) //2nd: write point to Matcher
+					preCon[GET] = false // switch case
 					break
 				case 1: // mouse event
-					def mEvent = mouseEvent.read()
-					if (mEvent.getID() == MouseEvent.MOUSE_PRESSED) {
-						preCon[GET] = true
-						def pointValue = mEvent.getPoint()
-						point = [(int)pointValue.x, (int)pointValue.y]
+					def mEvent = mouseEvent.read() //read from mouseEventChannel
+					if (mEvent.getID() == MouseEvent.MOUSE_PRESSED) { //if mouse pressed
+						preCon[GET] = true // switch case
+						def pointValue = mEvent.getPoint() // store point value
+						point = [(int)pointValue.x, (int)pointValue.y] // store as [x, y]
 					}
 					break
 			} // end of switch
